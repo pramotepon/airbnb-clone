07eadc32732a28b5b8f54cf70919d5cd41e70bcb
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-04-20 16:24:45 +0700

    feature/login

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 require('dotenv').config()
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'adasdsafwefijewfijwf';
 
 // Express parse json for res data to client
 app.use(express.json());
@@ -18,20 +20,38 @@ app.use(cors({
 }));
 
 // Install mongoose
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
-app.get('/test', (req,res) => {
+app.get('/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/register', async (req,res) => {
-    const {name, email, password} = req.body;
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
     const userDoc = await User.create({
         name,
         email,
-        password:bcrypt.hashSync(password, bcryptSalt),
+        password: bcrypt.hashSync(password, bcryptSalt),
     });
     res.json(userDoc);
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+                if(err) throw err;
+                res.cookie('token', token).json('pass ok');
+            });
+        } else {
+            res.status(422).json('pass failed');
+        }
+    } else {
+        res.json('Login failed');
+    }
 });
 
 // Call api project on port 4000
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react'
+import { Link, Navigate } from 'react-router-dom'
+import axios from 'axios'
 const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('/login', {email, password});
+      alert('Login Successfull');
+      setRedirect(true);
+    } catch (e){
+      alert('Login Failed');
+    }
+  }
+
+  if(redirect) {
+    return <Navigate to={'/'} />;
+  }
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-64'>
         <h1 className='text-4xl text-center mb-4'>Login</h1>
-        <form className='max-w-md mx-auto'>
-          <input type='email' placeholder='your@email.com' />
-          <input type='password' placeholder='password' />
+        <form className='max-w-md mx-auto' onSubmit={handleLoginSubmit}>
+          <input type='email' placeholder='your@email.com' value={email} onChange={e => setEmail(e.target.value)} />
+          <input type='password' placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
           <button className='primary'>Login</button>
           <div className='text-center py-2 text-gray-500'>
             Don't have an account yet? <Link className='underline text-black' to={'/register'}>Register now</Link>
